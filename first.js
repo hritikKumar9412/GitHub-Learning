b9c8e66d36badacde7146a5e1eada413aa927fdf
Hritik Kumar <hritikthakur200@gmail.com>
2026-03-06 15:01:18 +0530

    New Commit

--- first.js
+++ first.js
@@ -4,5 +4,8 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
-const foooter ="Fotter added in our website";
-console.log(foooter);
+const Payment = "Integerated the payment gateway";
+console.log(Payment);
+
+const upi ="Integrated the upi gateway";
+console.log(upi);
